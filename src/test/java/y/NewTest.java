925ed9163d63478ddbd0881930343d911fc05a47
Anotrer Ferma <bonmagaba@gmail.com>
2013-06-13 12:47:52 +0700

    Changed POM to fail builds on integration test failures
    Change-Id: Ie7f36800218f628d49032d38e36bacef0fa18756

--- src/test/java/y/NewTest.java
+++ src/test/java/y/NewTest.java
@@ -15,12 +15,10 @@ public class NewTest {
 	@Test
 	@Category(y.IntegrationTest.class)
 	public void emptyTest1() throws Exception {
-        //HtmlUnitDriver driver = new HtmlUnitDriver();
-		WebDriver driver = new FirefoxDriver();
+        HtmlUnitDriver driver = new HtmlUnitDriver();
+		//WebDriver driver = new FirefoxDriver();
         driver.get("http://www.azoft.com/");
 		Assert.assertEquals(driver.getTitle(), "Google");
 		driver.quit();
-        //WebElement query = driver.findElement(By.name("q"));
-		//System.out.println("Page title is: " + );
 	}
 }
